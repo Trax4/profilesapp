037dd8946b36f4ed21f43d2dc6ec170c03be9308
Trax4 <Davideneita@gmail.com>
2024-11-15 12:03:39 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function callAPI() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    alert(`Calling API with: First Name = ${firstName}, Last Name = ${lastName}`);
+    // Add code here to make an API call if necessary
+}
